9ab5d7432db6df5b5709fb291c3d4255363756fa
Rishi <rishipatel092005@gmail.com>
2026-06-11 20:34:31 +0530

    Fix frontend build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,7 @@ export default function App() {
   });
 
   // Fetch tickets
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     fetchTickets();
   }, [search, status]);
